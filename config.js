150ece3229ba4fa2cf3e5abdf8bed102619b07f3
devals21 <84735680+devals21@users.noreply.github.com>
2023-11-02 12:06:45 +0100

    Create config.js
    Google Calendar and Notion Config

---
+++ config.js
@@ -0,0 +1,36 @@
+//============================================================//
+//                SETTINGS FOR GOOGLE CALENDAR                //
+//============================================================//
+
+const CALENDAR_IDS = [
+  "primary", // Primary Local-181
+  getBirthDaysCalendarId(), // Birthdays
+  // Add all the calendar IDs you want to sync
+];
+
+//============================================================//
+
+
+//============================================================//
+//                    SETTINGS FOR NOTION                     //
+//============================================================//
+
+const DATABASE_ID = ""; // Notion Database ID
+const NOTION_TOKEN = ""; // Notion token 
+const NOTION_INTEGRATION_ID = ""; // Notion Integration ID
+
+// Name of your Notion Database Properties
+const NAME_NOTION = "Name";
+const DONE_NOTION = "Done";
+const DATE_NOTION = "Date";
+const DESCRIPTION_NOTION = "Description";
+const CALENDAR_NAME_NOTION = "Calendar Name";
+const EVENT_LINK_NOTION = "Event link";
+const IGNORE_NOTION = "Ignore Sync";
+const ARCHIVED_NOTION = "Archived";
+const LOCATION_NOTION = "Location";
+const EVENT_ID_NOTION = "event_id";
+const CALENDAR_ID_NOTION = "calendar_id";
+const EDITED_BY_NOTION = "last_edited_by";
+
+//============================================================//
