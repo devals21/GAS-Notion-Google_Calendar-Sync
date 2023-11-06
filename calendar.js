af3349d777b7a19e1fc413b78cca30a97fbc547d
devals21 <84735680+devals21@users.noreply.github.com>
2023-11-06 19:28:38 +0100

    Update calendar.js
    Added convertToGCalEvent and createEventDescription functions

--- calendar.js
+++ calendar.js
@@ -89,3 +89,78 @@ function getCalendarEvents(ev, fullSync = false) {
 }
 
 //============================================================//
+
+/**
+ * Return GCal event object based on page properties
+ * @param {Object} page - Notion page result object
+ * @returns {Object} - GCal event object Return False if required properties not found
+ */
+function convertToGCalEvent(page) {
+  let e_id = page.properties[EVENT_ID_NOTION].rich_text;
+  e_id = Utils.flattenRichText(e_id);
+
+  let e_summary = page.properties[NAME_NOTION].title;
+  e_summary = removeDoneInTitle(Utils.flattenRichText(e_summary));
+  e_summary = e_summary == "" ? "UNNAMED" : e_summary;
+
+  if (page.properties[DONE_NOTION].checkbox == true) e_summary = setDoneInTitle(e_summary);
+
+  let e_description = createEventDescription(page);
+
+  let e_location = page.properties[LOCATION_NOTION].rich_text;
+  e_location = Utils.flattenRichText(e_location);
+
+  let dates = page.properties[DATE_NOTION];
+
+  if (dates.date) {
+    let all_day = dates.date.end === null;
+    let default_end;
+
+    if (dates.date.start && dates.date.start.search(/([A-Z])/g) === -1) {
+      dates.date.start += "T00:00:00";
+      all_day = true;
+
+      default_end = new Date(dates.date.start);
+      default_end.setDate(default_end.getDate() + 1);
+      dates.date.end = default_end.toISOString();
+    } else if (
+      !dates.date.end &&
+      dates.date.start &&
+      dates.date.start.search(/([A-Z])/g) !== -1
+    ) {
+      all_day = false;
+      default_end = new Date(dates.date.start);
+      default_end.setMinutes(default_end.getMinutes() + 30);
+
+      dates.date.end = default_end.toISOString();
+    } else if (dates.date.end && dates.date.end.search(/([A-Z])/g) === -1) {
+      dates.date.end += "T00:00:00";
+      all_day = true;
+    }
+
+    let start = all_day ? { date: dates.date.start, timeZone: Session.getScriptTimeZone() } : { dateTime: dates.date.start, timeZone: Session.getScriptTimeZone() };
+    let end = all_day ? { date: dates.date.end, timeZone: Session.getScriptTimeZone() } : { dateTime: dates.date.end, timeZone: Session.getScriptTimeZone() };
+
+    let event = {
+      ...(e_id && { id: e_id }),
+      ...(e_summary && { summary: e_summary }),
+      ...(e_description && { description: e_description }),
+      ...(e_location && { location: e_location }),
+      ...(dates.date.start && { start: start }),
+      ...(dates.date.end && { end: end }),
+      all_day: all_day,
+    };
+
+    return event;
+  } else {
+    return false;
+  }
+}
+
+
+function createEventDescription(page) {
+  return "Description: "
+    .concat(Utils.flattenRichText(page.properties[DESCRIPTION_NOTION].rich_text))
+    .concat("\nNotion-Link: ")
+    .concat(page.url)
+}
